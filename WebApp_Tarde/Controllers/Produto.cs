4da7c4db398b6b3daeed1d41aa762c73457bce36
biagalvess <Belatrix12>
2023-04-26 15:59:42 -0300

    Aula dia 26/04

---
+++ WebApp_Tarde/Controllers/Produto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Tarde.Controllers
+{
+    public class Produto : Controller
+    {
+        private readonly Contexto db;
+
+        public Produto(Contexto opt)
+        {
+            db = opt;
+        }
+
+        public IActionResult Lista()
+        {
+            return View(db.Produtos.ToList());
+        }
+    }
+}
+   
---
+++ WebApp_Tarde/Entidades/ProdutoEntidade.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WebApp_Tarde.Entidades
+{
+    public class ProdutoEntidade
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor{ get; set; }
+        public bool Ativo { get; set; }
+}
+}
